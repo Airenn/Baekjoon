4ee7576acf537c94b24e6497352f00cc36ea2cd4
Airen <qga5594@gmail.com>
2021-03-14 17:56:15 +0900

    210314

---
+++ Baekjoon_C/while문/1110.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, a, b, count = 0;
+
+    scanf("%d", &num1);
+    num2 = num1;
+
+    do
+    {
+        a = num1 / 10;
+        b = num1 % 10;
+        num1 = b * 10 + ((a + b) % 10);
+        count++;
+    } while (num1 != num2);
+    printf("%d", count);
+}
